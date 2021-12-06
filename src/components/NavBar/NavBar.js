f50b4e435cb67185df7f3bd474e74c5e872a14ac
Jakub Prymak <jprymak@gmail.com>
2021-12-06 18:56:42 +0100

    refactor: replace hardcoded links with mapped object array

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,18 +3,15 @@ import { GiMagnifyingGlass } from "react-icons/gi";
 
 import "./index.css";
 
+import {links} from "data/links";
+
 function NavBar() {
   return (
     <nav className="nav">
           <GiMagnifyingGlass />
           <h1 className="nav-app-name">Wiki-search</h1>
       <ul className="nav-list">
-        <li>
-          <Link to="/">Search Articles</Link>
-        </li>
-        <li>
-          <Link to="/history">History</Link>
-        </li>
+        {links.map(link=><li key={link.id}><Link to={link.url}>{link.page}</Link></li>)}
       </ul>
     </nav>
   );
---
+++ src/data/links.js
@@ -0,0 +1,13 @@
+export const links = [
+    {
+        id: 0,
+        page: "Search articles",
+        url: "/"
+    },
+    {
+        id: 1,
+        page: "History",
+        url: "/history"
+    }
+
+]
